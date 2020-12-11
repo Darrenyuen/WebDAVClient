b5159041b951fedbc1d6eb2ed55b3ef84d5effbd
yuan <yuansssf@gmail.com>
2020-12-11 10:11:00 +0800

    upload module

---
+++ uploader/src/main/java/com/darrenyuen/uploader/Uploader.java
@@ -0,0 +1,8 @@
+package com.darrenyuen.uploader;
+
+/**
+ * Create by yuan on 2020/12/10
+ */
+public class Uploader {
+
+}
---
+++ uploader/src/main/java/com/darrenyuen/uploader/UploaderListener.java
@@ -0,0 +1,7 @@
+package com.darrenyuen.uploader;
+
+/**
+ * Create by yuan on 2020/12/10
+ */
+public interface UploaderListener {
+}
