e3a55e8eced5bf347fa0228555a6bb9d98b71e10
yuan <yuansssf@gmail.com>
2021-03-04 15:27:15 +0800

    可删除服务器上的文件

--- app/src/main/java/com/darrenyuen/webdavclient/FileContainerAdapter.kt
+++ app/src/main/java/com/darrenyuen/webdavclient/FileContainerAdapter.kt
@@ -2,63 +2,119 @@ package com.darrenyuen.webdavclient
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.media.Image
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.darrenyuen.webdavclient.widget.BottomDialog
+import com.thegrizzlylabs.sardineandroid.impl.OkHttpSardine
 
 /**
  * Create by yuan on 2021/2/28
  */
-class FileContainerAdapter(private val context: Context, private var fileList: List<FileBean>) : RecyclerView.Adapter<FileContainerAdapter.ViewHolder>() {
+class FileContainerAdapter(private val mContext: Context, private var mNode: FileTreeNode) : RecyclerView.Adapter<FileContainerAdapter.ViewHolder>() {
 
     val TAG = "FileContainerAdapter"
 
     private var mOnItemClickListener: OnItemClickListener? = null
+
+    private var mBottomDialog: BottomDialog? = null
+    private val mHandler: Handler = Handler(Looper.getMainLooper())
 
     fun setOnItemClickListener(onItemClickListener: OnItemClickListener) {
         mOnItemClickListener = onItemClickListener
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_file, parent, false))
+        return ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.item_file, parent, false))
     }
 
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        Log.i(TAG, fileList[position].path)
+        Log.i(TAG, mNode.mChildren[position].mValue.path)
         var fileType = FileType.File
-        if (fileList[position].name.contains(".txt")) {
+        if (mNode.mChildren[position].mValue.name.contains(".txt")) {
             holder.iconFileTypeIV.setImageResource(R.drawable.txt)
-        } else if (fileList[position].path.contains(".pdf")) {
+        } else if (mNode.mChildren[position].mValue.name.contains(".pdf")) {
             holder.iconFileTypeIV.setImageResource(R.drawable.pdf)
-        } else if (fileList[position].path.contains(".apk")) {
+        } else if (mNode.mChildren[position].mValue.name.contains(".apk")) {
             holder.iconFileTypeIV.setImageResource(R.drawable.apk)
+        } else if (mNode.mChildren[position].mValue.name.contains(".doc")) {
+            holder.iconFileTypeIV.setImageResource(R.drawable.word)
         } else {
             holder.iconFileTypeIV.setImageResource(R.drawable.dir)
 //            holder.fileSizeTV.visibility = View.GONE
             fileType = FileType.Dir
         }
-        holder.fileNameTV.text = fileList[position].name
+        holder.fileNameTV.text = mNode.mChildren[position].mValue.name
         if (fileType == FileType.File) {
-            holder.fileSizeTV.text = fileList[position].size.toString().substring(0, fileList[position].size.toString().indexOf(".") + 2) + "MB"
+            holder.fileSizeTV.text = mNode.mChildren[position].mValue.size.toString().substring(0, mNode.mChildren[position].mValue.size.toString().indexOf(".") + 2) + "MB"
         } else if (fileType == FileType.Dir) holder.fileSizeTV.visibility = View.GONE
-        holder.fileTimeTV.text = fileList[position].lastModified.toString().replace("GMT+08:00 ", "")
+        holder.fileTimeTV.text = mNode.mChildren[position].mValue.lastModified.toString().replace("GMT+08:00 ", "")
         holder.itemView.setOnClickListener {
-            mOnItemClickListener?.onItemClick(fileList[position].path, fileType)
+            mOnItemClickListener?.onItemClick(mNode.mChildren[position], fileType)
+        }
+        holder.moreIV.setOnClickListener {
+            mBottomDialog = BottomDialog.Builder(mContext)
+                    .title("请选择操作：")
+                    .orientation(BottomDialog.VERTICAL)
+                    .menu(R.menu.item_bottom_dialog)
+                    .padding(5)
+                    .paddingInItem(10)
+                    .itemSize(18)
+                    .onItemClickListener {
+                        when (it.id) {
+                            R.id.rename -> webDavOperation(WebDavOperation.Rename, mNode.mChildren[position].mValue.path)
+                            R.id.copy -> webDavOperation(WebDavOperation.Copy, mNode.mChildren[position].mValue.path)
+                            R.id.detail -> webDavOperation(WebDavOperation.Detail, mNode.mChildren[position].mValue.path)
+                            R.id.delete -> webDavOperation(WebDavOperation.Delete, mNode.mChildren[position].mValue.path)
+                        }
+                    }
+                    .build()
+            mBottomDialog?.show()
         }
     }
 
-    override fun getItemCount(): Int {
-        return fileList.size
+    private fun webDavOperation(operation: WebDavOperation, path: String) {
+        when(operation) {
+//            WebDavOperation.Rename -> {
+//                Thread {
+//                    val sardine = OkHttpSardine()
+//                    sardine.setCredentials("dev", "yuan")
+//                    Log.i(TAG, "http://119.29.176.115$path")
+//                    sardine.
+//                }.start()
+            WebDavOperation.Delete -> {
+                Thread {
+                    val sardine = OkHttpSardine()
+                    sardine.setCredentials("dev", "yuan")
+                    sardine.delete("http://119.29.176.115$path")
+                    runOnUIThread {
+                        Toast.makeText(mContext, "删除成功", Toast.LENGTH_SHORT).show()
+                        mBottomDialog?.dismiss()
+                        notifyDataSetChanged()
+                    }
+                }.start()
+            }
+        }
     }
 
-    fun setData(data: List<FileBean>) {
-        fileList = data
+    private fun runOnUIThread(action: () -> Unit) {
+        mHandler.post(action)
+    }
+
+    override fun getItemCount(): Int {
+        return mNode.mChildren.size
+    }
+
+    fun setData(node: FileTreeNode) {
+        mNode = node
         notifyDataSetChanged()
     }
 
@@ -68,10 +124,11 @@ class FileContainerAdapter(private val context: Context, private var fileList: L
         val fileNameTV = itemView.findViewById<TextView>(R.id.fileName)
         val fileSizeTV = itemView.findViewById<TextView>(R.id.fileSize)
         val fileTimeTV = itemView.findViewById<TextView>(R.id.time)
+        val moreIV = itemView.findViewById<ImageView>(R.id.more)
     }
 
     interface OnItemClickListener {
-        fun onItemClick(path: String, type: FileType)
+        fun onItemClick(node: FileTreeNode, type: FileType)
     }
 
 }
---
+++ app/src/main/java/com/darrenyuen/webdavclient/WebDavOperation.kt
@@ -0,0 +1,12 @@
+package com.darrenyuen.webdavclient
+
+/**
+ * Create by yuan on 2021/3/4
+ */
+enum class WebDavOperation {
+    Rename,
+    Move,
+    Copy,
+    Detail,
+    Delete
+}
